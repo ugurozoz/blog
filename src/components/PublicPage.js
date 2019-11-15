bbe1da21fd28b45cb42b85cd1f47446af73b5621
ugur ozoz <ugurozoz@yahoo.com>
2019-11-15 09:21:36 +0300

    public routes update(in progress)

--- src/components/PublicPage.js
+++ src/components/PublicPage.js
@@ -2,23 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
-export const PublicPage = ({ startLogin }) => (
+export const PublicPage = () => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Boilerplate App</h1>
-      <p>Boilerplate App Description</p>
-      <button className="button" onClick={startLogin}>
-        Login
-      </button>
+      <p>Public Page</p>
     </div>
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-  startLogin: () => dispatch(startLogin())
-});
-
-export default connect(
-  undefined,
-  mapDispatchToProps
-)(PublicPage);
+export default PublicPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,12 +6,11 @@ import NotFoundPage from "../components/NotFoundPage";
 import EditPostPage from "../components/EditPostPage";
 import ShowPostPage from "../components/ShowPostPage";
 import CreatePostPage from "../components/CreatePostPage";
-import PublicPage from "../components/PublicPage";
 import Header from "../components/Header";
 import LoginPage from "../components/LoginPage";
+import PublicPage from "../components/PublicPage";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
-
 
 export const history = createHistory();
 
@@ -19,14 +18,12 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/public" component={PublicPage}  exact={true}/>
-        <PublicRoute path="/" component={LoginPage} exact={true}  />   
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={DashboardPage} />
         <PrivateRoute path="/create" component={CreatePostPage} />
         <PrivateRoute path="/edit/:id" component={EditPostPage} />
-        <PrivateRoute path="/post/:id" component={ShowPostPage} />        
+        <PrivateRoute path="/post/:id" component={ShowPostPage} />
         <Route component={NotFoundPage} />
-        
       </Switch>
     </div>
   </Router>
