becadec8b4bf2ea90d3c48a07ad927149c862b45
ugur ozoz <ugurozoz@yahoo.com>
2020-02-06 13:24:01 +0300

    push test for jenkins

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Blog App</h1>
-      <p>Login Page</p>
+      <p>login page</p>
       <button className="button" onClick={startLogin}>
         Login
       </button>
